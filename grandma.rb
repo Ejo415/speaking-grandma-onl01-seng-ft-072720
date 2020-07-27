18bc7725d00048d821d38c42e0e24f75d8e965b9
Eugene O'Neil <ejo7220@gmail.com>
2020-07-27 20:51:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 
-def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-    elsif string.downcase
+    elsif phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  else string.upcase
+  else phrase.upcase
     puts "NO, NOT SINCE 1938!"
 end
